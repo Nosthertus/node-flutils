dd0b9aae5ad92000f33578f0aa8a2524cc0861c3
Nosthertus <darkfreak17@hotmail.com>
2016-05-02 13:26:07 -0430

    NEW: package
    *Added package file

---
+++ index.js
@@ -0,0 +1,67 @@
+var fs   = require('fs');
+var path = require('path');
+
+var flutils = {
+	/**
+	 * Load JSON file and return parsed JSON
+	 * 
+	 * @param  {String} 	  file 	Directory of the file
+	 * @return {Object|Array}      	Parsed JSON
+	 */
+	loadJSON: function(file){
+		var data = fs.readFileSync(path.normalize(file));
+
+		return JSON.parse(data);
+	},
+
+	/**
+	 * Load All files in a directory
+	 * 
+	 * @param  {String} dir  Directory of the files to load
+	 * @param  {Object} opts Options for the load
+	 * @return {Object}      The object with name and content of the files
+	 */
+	loadAll: function(dir, opts){
+		var options = opts || {};
+
+		var files = flutils.readDir(dir, options);
+
+		var filesObj = new Object;
+
+		for(var i = 0; i < files.length; i++){
+			if(options.type == 'json')
+				filesObj[files[i]] = flutils.loadJSON(path.join(dir, files[i]));
+		}
+
+		return filesObj
+	},
+
+	/**
+	 * Read the contents of a directory
+	 * 
+	 * @param  {String} dir  Directory to read
+	 * @param  {Object} opts Options for the read
+	 * @return {Array}       The list of the files contained in an object
+	 */
+	readDir: function(dir, opts){
+		var options = opts || {};
+
+		var directory = fs.readdirSync(path.normalize(dir));
+
+		return directory.filter(function(file){
+			return (opts.type && opts.type == flutils.normalizeExt(path.extname(file)));
+		});
+	},
+
+	/**
+	 * Normalize an extension name, removig the dot
+	 * 
+	 * @param  {String} ext The extension/file to normalize
+	 * @return {String}     The normalized extension
+	 */
+	normalizeExt: function(ext){
+		return ext.split('.')[1];
+	}
+};
+
+module.exports = flutils;
